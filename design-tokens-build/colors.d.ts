6547a97de4ad66c1eec5d499af643c6a19847f6a
Roy Halvor Frimanslund <5771598+Camulos@users.noreply.github.com>
2025-12-01 14:17:55 +0100

    Config + nye profilfarger
    Oppdateringen er som en del av prosessen ved å etablere ny oppfrisket profil for BR, og at vi skal ta i bruk Config-fil for design-tokens.

---
+++ design-tokens-build/colors.d.ts
@@ -0,0 +1,23 @@
+/* This file is deprecated and will be removed in a future release. Use types.d.ts instead */
+/* build: v1.7.3 */
+import type {} from '@digdir/designsystemet/types';
+
+// Augment types based on theme
+declare module '@digdir/designsystemet/types' {
+  export interface ColorDefinitions {
+    main1: never;
+    main2: never;
+    main3: never;
+    maintest: never;
+    support1: never;
+    support2: never;
+    supporttest: never;
+    neutral: never;
+  }
+  export interface SeverityColorDefinitions {
+    info: never;
+    success: never;
+    warning: never;
+    danger: never;
+  }
+}
